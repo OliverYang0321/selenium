b115b6f50a9130cbf032ecf7a8be8b01cda3e9d4
Oliver Yang <oliver.yang@simprogroup.com>
2024-02-18 14:17:10 +1000

    duplicate maximized mode

--- TestSelenium/src/test/java/TestCaseThree.java
+++ TestSelenium/src/test/java/TestCaseThree.java
@@ -162,7 +162,6 @@ public class TestCaseThree {
 		// Test on Chrome
 		ChromeOptions optionsChrome = new ChromeOptions();
 		optionsChrome.addArguments("--start-maximized"); // open Browser in maximized mode
-		optionsChrome.addArguments("--window-size=1920,1080"); // open Browser in maximized mode
 		optionsChrome.addArguments("--headless=new");
 		WebDriver driverChrome = new ChromeDriver(optionsChrome);
 		differentBrowsersTest(driverChrome);
--- TestSelenium/src/test/java/TestCaseTwo.java
+++ TestSelenium/src/test/java/TestCaseTwo.java
@@ -68,7 +68,6 @@ public class TestCaseTwo {
 		// Test on Chrome
 		ChromeOptions optionsChrome = new ChromeOptions();
 		optionsChrome.addArguments("--start-maximized"); // open Browser in maximized mode
-		optionsChrome.addArguments("--window-size=1920,1080"); // open Browser in maximized mode
 		optionsChrome.addArguments("--headless=new");
 		WebDriver driverChrome = new ChromeDriver(optionsChrome);
 		differentBrowsersTest(driverChrome);
